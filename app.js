c792e71ef87326c78c5a6e5bc2340f879002583b
Aditya Tiwari <aditiwari0307@gmail.com>
2025-09-07 04:11:02 +0530

    Fix port handling for Render deployment and update routes

--- app.js
+++ app.js
@@ -102,6 +102,10 @@ app.use("/listings/:id/reviews",reviewRouter)
 
 
 app.use("/",userRouter);
+app.get("/", (req, res) => {
+    res.redirect("/listings"); // or render a homepage
+});
+
 
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
